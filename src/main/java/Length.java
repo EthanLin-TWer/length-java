c7f37da37509aa75ad32e998adafd1b0a2b7316b
Linesh <linesh.simpcity@gmail.com>
2020-01-13 07:26:18 +0800

    refactor: replace unused getUnit()

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -60,10 +60,6 @@ public class Length {
         return this.value;
     }
 
-    public String getUnit() {
-        return this.unit;
-    }
-
     public Unit getTemp_unit() {
         return temp_unit;
     }
