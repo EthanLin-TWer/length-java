6f7c8c83d17f95ac9624106fed72ac70dde6c4ae
Linesh <linesh.simpcity@gmail.com>
2020-01-13 07:37:19 +0800

    refactor: use 'Save Delete' action on unused constructor unit param with option + enter

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -5,7 +5,7 @@ public class Length {
     public static final String YARD = "yard";
     public static final String INCH = "inch";
 
-    public Length(double value, String unit, Unit temp_unit) {
+    public Length(double value, Unit temp_unit) {
         this.value = value;
         this.temp_unit = temp_unit;
     }
@@ -15,25 +15,25 @@ public class Length {
 
         if (this.temp_unit == Unit.FOOT) {
             if (target == Unit.YARD) {
-                result = new Length(this.value / 3, null, target);
+                result = new Length(this.value / 3, target);
             } else if (target == Unit.INCH) {
-                result = new Length(this.value * 12, null, target);
+                result = new Length(this.value * 12, target);
             }
         }
 
         if (this.temp_unit == Unit.YARD) {
             if (target == Unit.INCH) {
-                result = new Length(this.value * 36, null, target);
+                result = new Length(this.value * 36, target);
             } else if (target == Unit.FOOT){
-                result = new Length(this.value * 3, null, target);
+                result = new Length(this.value * 3, target);
             }
         }
 
         if (this.temp_unit == Unit.INCH) {
             if (target == Unit.FOOT) {
-                result = new Length(this.value / 12, null, target);
+                result = new Length(this.value / 12, target);
             } else if (target == Unit.YARD) {
-                result = new Length(this.value / 36, null, target);
+                result = new Length(this.value / 36, target);
             }
         }
 
