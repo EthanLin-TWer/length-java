bf222a23163dc3d4bb5b0639a645174d69569158
Linesh <linesh.simpcity@gmail.com>
2020-01-13 07:17:00 +0800

    refactor: replace some more references to temp_as()

--- src/test/java/LengthTest.java
+++ src/test/java/LengthTest.java
@@ -14,7 +14,7 @@ public class LengthTest {
 
     @Test
     public void should_2_feet_equals_2_feet() {
-        Length result = new Length(2, Length.FOOT).as(Length.FOOT);
+        Length result = new Length(2, Length.FOOT).temp_as(Length.FOOT, Unit.FOOT);
 
         assertThat(result.getValue(), is(2.0));
         assertThat(result.getUnit(), is(Length.FOOT));
@@ -22,7 +22,7 @@ public class LengthTest {
 
     @Test
     public void should_1_yard_equals_1_yard() {
-        Length result = new Length(1, Length.YARD).as(Length.YARD);
+        Length result = new Length(1, Length.YARD).temp_as(Length.YARD, Unit.YARD);
 
         assertThat(result.getValue(), is(1.0));
         assertThat(result.getUnit(), is(Length.YARD));
@@ -30,7 +30,7 @@ public class LengthTest {
 
     @Test
     public void should_1_foot_equals_12_inches() {
-        Length result = new Length(1, Length.FOOT).as(Length.INCH);
+        Length result = new Length(1, Length.FOOT).temp_as(Length.INCH, Unit.INCH);
 
         assertThat(result.getValue(), is(12.0));
         assertThat(result.getUnit(), is(Length.INCH));
@@ -38,7 +38,7 @@ public class LengthTest {
 
     @Test
     public void should_3_foot_equals_1_yard() {
-        Length result = new Length(3, Length.FOOT).as(Length.YARD);
+        Length result = new Length(3, Length.FOOT).temp_as(Length.YARD, Unit.YARD);
 
         assertThat(result.getValue(), is(1.0));
         assertThat(result.getUnit(), is(Length.YARD));
@@ -46,7 +46,7 @@ public class LengthTest {
 
     @Test
     public void should_1_yard_equals_3_feet() {
-        Length result = new Length(1, Length.YARD).as(Length.FOOT);
+        Length result = new Length(1, Length.YARD).temp_as(Length.FOOT, Unit.FOOT);
 
         assertThat(result.getValue(), is(3.0));
         assertThat(result.getUnit(), is(Length.FOOT));
