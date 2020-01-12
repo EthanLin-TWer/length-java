38b5d62fec817c44b43a5708bb3257b4a1da2780
Linesh <linesh.simpcity@gmail.com>
2020-01-13 07:43:31 +0800

    refactor: remove unused result variable

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -9,32 +9,26 @@ public class Length {
 
     public Length as(Unit target) {
         double value = this.value;
-        Length result = new Length(value, target);
 
         if (this.unit == Unit.FOOT) {
             if (target == Unit.YARD) {
                 value = this.value / 3;
-                result = new Length(value, target);
             } else if (target == Unit.INCH) {
                 value = this.value * 12;
-                result = new Length(value, target);
             }
         }
 
         if (this.unit == Unit.YARD) {
             if (target == Unit.INCH) {
                 value = this.value * 36;
-                result = new Length(value, target);
             } else if (target == Unit.FOOT){
                 value = this.value * 3;
-                result = new Length(value, target);
             }
         }
 
         if (this.unit == Unit.INCH) {
             if (target == Unit.FOOT) {
                 value = this.value / 12;
-                result = new Length(value, target);
             } else if (target == Unit.YARD) {
                 value = this.value / 36;
             }
