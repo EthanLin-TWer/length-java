8098271f73ef27329e5848586c69ac61be556c20
来一 <tom.lai19871106@gmail.com>
2020-02-01 12:36:18 +0800

    创建Unit枚举类型，作为temp_as传入参数

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -11,10 +11,10 @@ public class Length {
     }
 
     public Length as(String targetUnit) {
-        return temp_as(targetUnit);
+        return temp_as(targetUnit, null);
     }
 
-    public Length temp_as(String targetUnit) {
+    public Length temp_as(String targetUnit, Unit tempUnit) {
         Length len = this;
         if (this.unit.equals(Length.FOOT)) {
             if (targetUnit.equals(Length.YARD)) {
---
+++ src/main/java/Unit.java
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lai.yi
+ * Date: 2020/2/1
+ * Description:
+ **/
+public enum Unit {
+}
