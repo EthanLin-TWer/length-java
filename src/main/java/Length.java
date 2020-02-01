75c5a1832f1a4ee63eaf56d8ec7c0dcd0b67c7c9
来一 <tom.lai19871106@gmail.com>
2020-02-01 12:32:16 +0800

    提取字符串常量

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,4 +1,7 @@
 public class Length {
+    private static final String YARD = "yard";
+    private static final String FOOT = "f";
+    private static final String INCH = "inch";
     private final double value;
     private final String unit;
 
@@ -9,26 +12,26 @@ public class Length {
 
     public Length as(String u) {
         Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
+        if (this.unit.equals(Length.FOOT)) {
+            if (u.equals(Length.YARD)) {
                 len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
+            } else if (u.equals(Length.INCH)) {
                 len = new Length(this.value * 12, u);
             }
         }
 
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
+        if (this.unit.equals(Length.YARD)) {
+            if (u.equals(Length.INCH)) {
                 len = new Length(this.value * 36, u);
-            } else if (u.equals("f")) {
+            } else if (u.equals(Length.FOOT)) {
                 len = new Length(this.value * 3, u);
             }
         }
 
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
+        if (this.unit.equals(Length.INCH)) {
+            if (u.equals(Length.FOOT)) {
                 len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
+            } else if (u.equals(Length.YARD)) {
                 len = new Length(this.value / 36, u);
             }
         }
